d9bfce71d37c0cc0597428ab729fe7590a1aff78
Robert Booster <boosterdev@users.noreply.github.com>
2018-11-19 08:28:39 +0100

    Update DENames.java

--- src/main/java/com/theundertaker11/moreavaritia/compat/draconicevolution/DENames.java
+++ src/main/java/com/theundertaker11/moreavaritia/compat/draconicevolution/DENames.java
@@ -7,7 +7,7 @@ public class DENames {
 	private static final String id = Reference.DraconicEvolutionModID+":";
 	public static final String AWAKENED_CORE = id+"awakened_core";
 	public static final String CHAOTIC_CORE = id+"chaotic_core";
-	public static final String CRAFTING_INJECTOR = id+"crafting_pedestal";
+	public static final String CRAFTING_INJECTOR = id+"crafting_injector";
 	public static final String REACTOR_PART = id+"reactor_component";
 	public static final String CAPACITOR = id+"draconium_capacitor";
 	public static final String REACTOR_CORE = id+"reactor_core";
