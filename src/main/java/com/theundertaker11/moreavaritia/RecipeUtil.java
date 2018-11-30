b39fe089700aba00a1d85b7d1690938018e7b030
Rafael Orman <Kronos9247@users.noreply.github.com>
2018-11-30 20:35:10 +0100

    Add files via upload

--- src/main/java/com/theundertaker11/moreavaritia/RecipeUtil.java
+++ src/main/java/com/theundertaker11/moreavaritia/RecipeUtil.java
@@ -16,24 +16,19 @@ public class RecipeUtil {
 	public static void addRecipe(ItemStack result, Object...objects) {
 		Object[] list = new Object[objects.length + 1];
 		list[0] = (Object) false;
-		
 		System.arraycopy(objects, 0, list, 1, objects.length);
 		
 		ShapedPrimer primer = CraftingHelper.parseShaped(list);
 		primer.mirrored = false;
 		
 		ExtremeShapedRecipe recipe = new ExtremeShapedRecipe(result, primer);
-		
 		ResourceLocation key = result.getItem().getRegistryName();
-//		ExtremeCraftingManager.REGISTRY.putObject(key, recipe.setRegistryName(key));
 		AvaritiaRecipeManager.EXTREME_RECIPES.put(key, recipe.setRegistryName(key));
 	}
 	
 	public static void addCatalystInput(ItemStack in) {		
 		ResourceLocation loc = new ResourceLocation(Avaritia.MOD_ID, "infinity_catalyst");
 		IExtremeRecipe recipe = ExtremeCraftingManager.REGISTRY.getObject(loc);
-		
-		System.out.println(recipe);
 		
 		if(recipe != null)
 			recipe.getIngredients().add(CraftingHelper.getIngredient(in));
