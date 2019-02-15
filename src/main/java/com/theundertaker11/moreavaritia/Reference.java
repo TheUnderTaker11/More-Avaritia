d8af2e5c399b5477e43f49a24ba4f8aa52d753bb
Under <cabmanbellou@hotmail.com>
2019-02-15 15:12:29 -0600

    Fixed mods not being all lowercase
    Probably will fix more of the crashing errors people can get when missing these mods.

--- src/main/java/com/theundertaker11/moreavaritia/Reference.java
+++ src/main/java/com/theundertaker11/moreavaritia/Reference.java
@@ -2,22 +2,22 @@ package com.theundertaker11.moreavaritia;
 
 public class Reference {
 	public static final String MODID = "moreavaritia";
-	public static final String VERSION = "2.4";
+	public static final String VERSION = "2.41";
 	public static final String NAME = "More Avaritia";
 	public static final String MCVersions = "1.12.2";
 	public static final String CLIENTPROXY = "com.theundertaker11.moreavaritia.proxy.ClientProxy";
 	public static final String SERVERPROXY = "com.theundertaker11.moreavaritia.proxy.CommonProxy";
 	
 	public static final String AppliedEnergisticsModID = "appliedenergistics2";
-	public static final String BotaniaModID = "Botania";//TODO change in 1.11
+	public static final String BotaniaModID = "botania";
 	public static final String CyberwearModID = "cyberware";
 	public static final String DraconicEvolutionModID = "draconicevolution";
-	public static final String EnderIOModID = "EnderIO";//TODO change in 1.11
+	public static final String EnderIOModID = "enderio";
 	public static final String ExtraUtilsModID = "extrautils2";
-	public static final String PSIModID = "Psi";//TODO change in 1.11
+	public static final String PSIModID = "psi";
 	public static final String QuantumFluxModID = "quantumflux";
 	public static final String RefinedStorageModID = "refinedstorage";
-	public static final String StorageDrawersModID = "StorageDrawers";//TODO change in 1.11
+	public static final String StorageDrawersModID = "storagedrawers";
 	public static final String ThermalExpansionModID = "thermalexpansion";
 	public static final String TinkersModID = "tconstruct";
 }
